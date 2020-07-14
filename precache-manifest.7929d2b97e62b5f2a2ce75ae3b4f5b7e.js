5521c98880ab8c407a9e657e779bf400f6089592
Ali Hussien <ali-18121@hotmail.com>
2020-07-14 17:17:00 +0200

    Updates

---
+++ precache-manifest.7929d2b97e62b5f2a2ce75ae3b4f5b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876605f753197644940c3b9c3116ef5e",
+    "url": "/TodaysGames/index.html"
+  },
+  {
+    "revision": "01e5ed1682d4380977c5",
+    "url": "/TodaysGames/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bd96d9e6ffda6cd183c4",
+    "url": "/TodaysGames/static/js/2.adb32f60.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodaysGames/static/js/2.adb32f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e5ed1682d4380977c5",
+    "url": "/TodaysGames/static/js/main.0bbdaa2d.chunk.js"
+  },
+  {
+    "revision": "3d95fa90caa4bae9f2d6",
+    "url": "/TodaysGames/static/js/runtime-main.bdfcf9a1.js"
+  }
+]);
